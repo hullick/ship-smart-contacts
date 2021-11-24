2117fc614ef0bab07a9dc1f41f6340f419e69cda
Hullick Bartholo Gomes <hullick02@gmail.com>
2021-11-24 18:14:15 -0300

    Added virtual scroller and contact avatar's component

---
+++ resources/js/components/ContactAvatar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="rounded-circle bg-danger text-center contact-avatar">
+    <span class="text-white fs-1 lh-1">{{ contact.name[0] }}</span>
+  </div>
+</template>
+<script>
+import { Contact } from "../classes/contact.ts";
+
+export default {
+  props: {
+    contact: {
+      type: Contact,
+      required: true,
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.contact-avatar {
+  min-height: 48px;
+  min-width: 48px;
+  max-height: 48px;
+  max-width: 48px;
+}
+</style>
